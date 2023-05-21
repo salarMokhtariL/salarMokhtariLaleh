47dcdab1895e12e2aa82c4e1197e19021004cb77
Salar mokhtarilaleh <salarmokhtari0@gmail.com>
2023-05-22 01:52:10 +0430

    Update 1.0.4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import "./App.css";
 import Header from './components/Header/Header';
+import Home from './components/home/Home';
 
 const App = () => {
   return (
     <>
     <Header/>
+
+    <main className='main'>
+      <Home/>
+    </main>
     </>
   )
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className='home__social'>
+        <a href='https://github.com/salarMokhtariL' className='home__social-icon' target="_blank">
+          <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a href='https://www.linkedin.com/in/salar-mokhtari-laleh-22508b91/' className='home__social-icon' target="_blank">
+            <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href='https://github.com/salarMokhtariL' className='home__social-icon' target="_blank">
+            <i class="uil uil-telegram-alt"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
